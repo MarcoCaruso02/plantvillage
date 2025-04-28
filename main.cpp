cb12010052287b60ff8db74c3885bf23344f2672
MarcoCaruso02 <marco.caruso-3@studentmail.unicas.it>
2025-04-28 19:44:12 +0200

    Trying region growing to segment

--- main.cpp
+++ main.cpp
@@ -1,52 +1,35 @@
 // include aia and ucas utility functions
 #include "ipaConfig.h"
 #include "ucasConfig.h"
-#include <opencv2/imgproc.hpp>
-// include my project functions
 #include "functions.h"
-
+#include <opencv2/photo.hpp>
 typedef std::vector <cv::Point> Contour;
-
+std::string root = "C:/Users/marco/Desktop/University/MAGISTRALE/1oANNO/2oSemestre/Image_Processing_Analysis/Codes/plants/dataset_less";
 
 namespace ipa {
+	cv::Mat img;
+
 	// bilateral filter parameters
 	int filter_size = 15;
 	int sigma_color = 150;
 	int sigma_space = 75;
 
 }
-template <typename Container, typename UnaryPredicate>
-void erase_if(Container& c, UnaryPredicate pred) {
-	c.erase(std::remove_if(c.begin(), c.end(), pred), c.end());
-}
-
-// Given an image it returns the contours found with Otsu or Triangle Binarization
-std::vector <Contour> contour_finder(cv::Mat img,bool Otsu) 
+cv::Mat createMarkersFromPixels(cv::Size size,
+	const std::vector<cv::Point>& foregroundPixels,
+	const std::vector<cv::Point>& backgroundPixels)
 {
-	if (Otsu)
-	{
-		int threshold = ucas::getOtsuAutoThreshold(ucas::histogram(img));
-		cv::threshold(img, img, threshold, 255, cv::THRESH_BINARY);
-		//ucas::imshow("Binarized image", channel);
+	cv::Mat markers = cv::Mat::zeros(size, CV_32S); // inizializza tutto a 0
 
-		std::vector <Contour> objects;
-		cv::findContours(img, objects, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
-		erase_if(objects, [](Contour& c) {
-			return cv::contourArea(c) < 500;});
-		return objects;
-	}
-	else
-	{
-		int threshold = ucas::getTriangleAutoThreshold(ucas::histogram(img));
-		cv::threshold(img, img, threshold, 255, cv::THRESH_BINARY);
-		//ucas::imshow("Binarized image", channel);
+	// Imposta pixel foreground (es. foglia) con marker ID 1
+	for (const auto& pt : foregroundPixels)
+		markers.at<int>(pt) = 255;
 
-		std::vector <Contour> objects;
-		cv::findContours(img, objects, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
-		erase_if(objects, [](Contour& c) {
-			return cv::contourArea(c) < 500;});
-		return objects;
-	}
+	// Imposta pixel background (es. sfondo) con marker ID 2
+	for (const auto& pt : backgroundPixels)
+		markers.at<int>(pt) = 2;
+
+	return markers;
 }
 
 int main()
@@ -55,66 +38,98 @@ int main()
 
 	try
 	{
-		
-		cv::Mat img = cv::imread(std::string(EXAMPLE_IMAGES_PATH) + "/dataset/Tomato___Bacterial_spot/Bacterial_spot_0.JPG");
+		cv::Mat img = cv::imread(root + "/Tomato___Bacterial_spot/Bacterial_spot_46.JPG");
 		if (!img.data)
 			throw ucas::Error("Cannot open image");
 
-		ucas::imshow("Original image", img);
+		//cv::resize(img, img, cv::Size(0, 0), 0.3f, 0.3f);
+
+		float magFactor = 1.0;
+
+		ucas::imshow("Original image", img, true, magFactor);
+
+
+		//denoise
+		cv::Mat img_denoised = img.clone();
+		cv::bilateralFilter(img, img_denoised, ipa::filter_size, ipa::sigma_color, ipa::sigma_space);
+		cv::imwrite(root + "/Tomato___Bacterial_spot/Bacterial_spot_0d.JPG", img_denoised);
+		ipa::imshow("denoised", img_denoised);
+
+
 		
-		// 1 - DENOISING
+		std::vector<cv::Point> foregroundPixels = { {103, 126}}; // foglia
+		std::vector<cv::Point> backgroundPixels = {};  // sfondo
+
+		cv::Size imgSize = cv::Size(256, 256); // dimensione immagine
+		cv::Mat seedsImg = createMarkersFromPixels(imgSize, foregroundPixels, backgroundPixels);
+
+
+	
+
+		// 1. Converti immagine in Lab
+		cv::Mat imgLab;
+		cv::cvtColor(img_denoised, imgLab, cv::COLOR_BGR2Lab);
+
+		// 2. Estrai il canale L
+		std::vector<cv::Mat> labChannels;
+		cv::split(imgLab, labChannels);
+		cv::Mat a = labChannels[1]; // a
+
+		cv::Mat hist;
+		int histSize = 256; // numero di bin
+		float range[] = { -128, 128 }; // intervallo di valori per il canale a
+		const float* histRange = { range };
+		cv::calcHist(&labChannels[1], 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, true, false);
+
+		// Mostra l'istogramma
+		int hist_w = 512, hist_h = 400;
+		int bin_w = cvRound((double)hist_w / histSize);
+		cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0, 0, 0));
+
+		normalize(hist, hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+		for (int i = 1; i < histSize; i++) {
+			line(histImage, cv::Point(bin_w * (i - 1), hist_h - cvRound(hist.at<float>(i - 1))),
+				cv::Point(bin_w * (i), hist_h - cvRound(hist.at<float>(i))),
+				cv::Scalar(255, 0, 0), 2, 8, 0);
+		}
+		cv::imshow("Histogram of a channel", histImage);
+		cv::waitKey(0);
+
+
+		// 3. Crea un criterio di segmentazione basato su a
+		int lowerBound = 50; // valore inferiore per il verde
+		int upperBound = 128;  // valore superiore per il verde
+
+		cv::Mat aCriterion;
+		cv::inRange(a, lowerBound, upperBound, aCriterion);
+
+
+		cv::Mat predicate = aCriterion;
+		ucas::imshow("aCriterion", aCriterion);
+
+		//region growing
+
+		cv::Mat seeds_curr;
+		seedsImg.convertTo(seeds_curr, CV_8U);
+		cv::Mat seeds_prev;
+		do
+		{
+			seeds_prev = seeds_curr.clone();
+
+			cv::Mat candidatePixels;
+			cv::dilate(seeds_curr, candidatePixels, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)));
+			candidatePixels = candidatePixels - seeds_curr;
+
+			seeds_curr += candidatePixels & predicate;
+
+			cv::imshow("Growing in progress", seeds_curr);
+			//cv::waitKey(50);
+
+		} while (cv::countNonZero(seeds_curr - seeds_prev));
+
+		ipa::imshow("result", seeds_curr);
 		
-		// bilateral filtering
-
-		cv::Mat denoised_img=img.clone();
-		cv::bilateralFilter(img, denoised_img, ipa::filter_size, ipa::sigma_color, ipa::sigma_space);
-
-		// 2 - SEGMENTATION
-		// 2.1 - Leaf is clearly visible in HSV color space (in particular in the first two channels)
-		// -> change color space 
-		cv::cvtColor(denoised_img, denoised_img, cv::COLOR_BGR2HSV);
-
-		std::vector <cv::Mat> imgChannels(3);
-		cv::split(denoised_img, imgChannels);
 		
-		//ucas::imshow("First channel", imgChannels[0]);
-		//ucas::imshow("Second channel", imgChannels[1]);
-		//ucas::imshow("Third channel", imgChannels[2]);
-		
-		
-		// The difference is considered in order to filter out other components
-		cv::Mat channel=imgChannels[0]-imgChannels[1];
-		// The sum is considered in order to clearly see the leaf 
-		cv::Mat channel_wl= imgChannels[0] + imgChannels[1];
-
-		//ucas::imshow("Channel", channel);
-		cv::Mat img_clone = img.clone();
-		
-		// 2.2 - Contours finding and drawing
-		std::vector<Contour> objects = contour_finder(channel, true);
-			
-		std::vector<Contour> objects_wl = contour_finder (channel_wl, true);
-
-		for (int k = 0; k < objects.size(); k++)
-			cv::drawContours(img, objects, k, cv::Scalar(0, 0,256), 1, cv::LINE_AA);
-
-		for (int k = 0; k < objects_wl.size(); k++)
-			cv::drawContours(img_clone, objects_wl, k, cv::Scalar(0, 0, 256), 1, cv::LINE_AA);
-
-		ucas::imshow("Contourn", img);
-
-		ucas::imshow("Contourn with leaf", img_clone);
-
-		// In certain cases the channel and channel_wl contain partial leaf's contours and external contours
-		// -> considering the difference leaves only the leaf
-		cv::Mat difference = img - img_clone;
-		cv::cvtColor(difference, difference, cv::COLOR_BGR2GRAY);
-		int triangle = ucas::getOtsuAutoThreshold(ucas::histogram(difference));
-		cv::threshold(difference, difference, triangle, 255, cv::THRESH_BINARY);
-
-		ucas::imshow("Difference", difference);
-
-
 		return EXIT_SUCCESS;
 	}
 	catch (ucas::Error& ex)
